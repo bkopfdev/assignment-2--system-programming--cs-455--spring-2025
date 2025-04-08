173c18d0714790b21f304cd85859a96c3d5bfcab
bkopfdev <bkopf8@gmail.com>
2025-04-08 18:29:57 -0400

    Submission

--- 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
+++ 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
@@ -1,1 +1,113 @@
-int main(){}
+#include <stdio.h>
+
+#define INPUT_ARRAY_SIZE 10
+
+void printArray(int const *, size_t);
+void bubbleSort(int[], int (*sortingMethod)(int *, int *), size_t);
+int ascending(int * const a, int * const b);
+int descending(int * const a, int * const b);
+void swap(int * const a, int * const b);
+
+int main() {
+  int a[INPUT_ARRAY_SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+  int userRequestedMethod; // 1 indicates ascending, 2 for descending
+
+  printf("Welcome to the Bubble Sorting program!\n");
+  printf("Please input the number corresponding to your preferred sorting method:\n1) Ascending\n2) Descending\n");
+  scanf("%d", &userRequestedMethod);
+
+  if(userRequestedMethod != 1 && userRequestedMethod != 2) {
+    printf("Invalid input. Please try again.\n");
+
+    return 1;
+  }
+
+  printf("\nYou have selected %s sorting.\n", userRequestedMethod == 1 ? "ASCENDING" : "DESCENDING");
+
+  printf("Array items in original order:\n");
+  printArray(a, INPUT_ARRAY_SIZE); // print the array using the function pointer
+
+  if(userRequestedMethod == 1) {
+    bubbleSort(a, &ascending, INPUT_ARRAY_SIZE); //if 1, pass ascending function address
+  } else {
+    bubbleSort(a, &descending, INPUT_ARRAY_SIZE); //if 2, pass descending function address
+  }
+
+  printf("\n\nArray items in sorted order:\n");
+  printArray(a, INPUT_ARRAY_SIZE); // print the array using the function pointer
+
+  putchar('\n');
+
+  return 0;
+}
+
+/**
+ * printArray()
+ * Prints the contents of an array of integers
+ *
+ * @array: pointer to the array of integers to be printed
+ * @arraySize: size of the array to be printed
+ */
+void printArray(int const * array, size_t arraySize) {
+  for(size_t i = 0; i < arraySize; i++) {
+    printf("%d ", array[i]);
+  }
+  putchar('\n');
+}
+
+/**
+ * ascending()
+ * Compares two integers by >
+
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ * @return: 1 if a is greater than b, 0 otherwise
+ */
+int ascending(int * a, int * b) {
+  return *a > *b;
+}
+
+/**
+ * descending()
+ * Compares two integers by <
+ *
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ * @return: 1 if a is less than b, 0 otherwise
+ */
+int descending(int * a, int * b) {
+  return *a < *b;
+}
+
+/**
+ * swap()
+ * Swaps two integers by reference
+ *
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ */
+void swap (int * a, int * b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+/**
+ * bubbleSort()
+ * sorts an array of integers using the bubble sort algorithm, in the order dictated by the function pointer
+ *
+ * @array: pointer to the array of integers to be sorted
+ * @param sortingMethodPtr: pointer to the sorting method function (ascending or descending)
+ * @param arraySize: size of the array to be sorted
+ */
+void bubbleSort(int array[], int (*sortingMethodPtr)(int *, int *), size_t arraySize) {
+  for(int pass = 0; pass < arraySize - 1; pass++) {
+
+    for(size_t j = 0; j < arraySize - 1; j++) {
+
+      if(sortingMethodPtr(&array[j], &array[j + 1]) > 0) {
+        swap(&array[j], &array[j + 1]);
+      }
+    }
+  }
+}
--- 02--arrays-of-function-pointers.c
+++ 02--arrays-of-function-pointers.c
@@ -1,1 +1,132 @@
-int main(){}
+#include <stdio.h>
+
+#define STUDENTS 3
+#define EXAMS 4
+
+void minimumGrade(int (*studentGradesArray)[EXAMS], size_t students, size_t exams);
+void maximumGrade(int (*studentGradesArray)[EXAMS], size_t students, size_t exams);
+void studentGradeAverage(int (*studentGradesArray)[EXAMS], size_t studentIndex, size_t exams);
+void printArray(int (*studentGradesArray)[EXAMS], size_t students, size_t exams);
+
+int main() {
+  int studentGrades[STUDENTS][EXAMS] = {
+    {77, 68, 86, 73},
+    {96, 87, 89, 78},
+    {70, 90, 86, 81}};
+
+  void (*processGrades[4])(int [][EXAMS], size_t, size_t) = {
+      printArray,
+      minimumGrade,
+      maximumGrade,
+      studentGradeAverage
+  };
+
+  int userInput = 0;
+  printf("Welcome to the Student Grades program!\n");
+
+  while(1)
+  {
+    printf("= = = = = = = = = = = = = = = \n"); //visual separator
+    printf("\nEnter a choice: \n\t0  Print the array of grades\n\t1  Find the minimum grade\n\t2  Find the maximum grade\n\t3  Print the average on all tests for each student\n\t4  End program\n");
+
+    if (scanf("%d", &userInput) != 1) { //if input is not an integer
+      printf("Invalid input. Please enter a number (0-4).\n");
+      while (getchar() != '\n'); // clear the input
+      continue;
+    }
+
+    printf("= = = = = = = = = = = = = = = \n"); //visual separator
+
+    if (userInput >= 0 && userInput < 4) {
+      processGrades[userInput](studentGrades, STUDENTS, EXAMS); // call the function pointer
+    } else if (userInput == 4) {
+      return 0;
+    } else {
+      printf("Invalid input. Please enter a number between 0 and 4.\n");
+    }
+  }
+
+  return 0;
+}
+
+/**
+ * minimumGrade()
+ * Finds the minimum grade out of all students, and then prints it
+ *
+ * @*studentGradesArray[]: Pointer to the 2D array of student grades
+ * @students: Number of students
+ * @exams: Number of exams
+ */
+void minimumGrade(int (*studentGradesArray)[EXAMS], size_t students, size_t exams) {
+  int min = **studentGradesArray; // Start with the first element
+  for (int *arrayPtr = &studentGradesArray[0][0]; arrayPtr < &studentGradesArray[students][0]; arrayPtr++) {
+      if (*arrayPtr < min) {
+          min = *arrayPtr;
+      }
+  }
+  printf("The minimum grade of all students is: %d\n", min);
+}
+
+/**
+ * maximumGrade()
+ * Finds the maximum grade out of all students and then prints it
+ *
+ * @*studentGradesArray[]: Pointer to the 2D array of student grades
+ * @students: Number of students
+ * @exams: Number of exams
+ */
+void maximumGrade(int (*studentGradesArray)[EXAMS], size_t students, size_t exams) {
+  int max = **studentGradesArray;
+  for (int *arrayPtr = &studentGradesArray[0][0]; arrayPtr < &studentGradesArray[students][0]; arrayPtr++) {
+    if (*arrayPtr > max) {
+        max = *arrayPtr;
+    }
+  }
+  printf("The maximum grade of all students is: %d\n", max);
+}
+
+/**
+ * studentGradeAverage()
+ * Calculates the average grade for each student, printing them out seperately
+ *
+ * @*studentGradesArray[]: Pointer to the 2D array of student grades
+ * @students: Number of students
+ * @exams: Number of exams
+ */
+void studentGradeAverage(int (*studentGradesArray)[EXAMS], size_t students, size_t exams) {
+  for (size_t i = 0; i < students; i++) {
+    float gradesSum = 0.0;
+    for (size_t i = 0; i < exams; i++) {
+      gradesSum += studentGradesArray[i][i];
+    }
+    printf("Student %zu average is: %.2f\n", i+1, (gradesSum / exams));
+  }
+}
+
+/**
+ * printArray()
+ * Prints the contents of a 2D array of integers
+ *
+ * @*studentGradesArray[]: Pointer to the 2D array of student grades
+ * @students: Number of students
+ * @exams: Number of exams
+ */
+void printArray(int (*studentGradesArray)[EXAMS], size_t students, size_t exams) {
+  printf("\t");
+  for (size_t i = 0; i < exams; i++) {
+    printf("\t[%zu]", i);
+  }
+
+  for (int i = 0; i < students; i++) {
+    printf("\nStudent %d: ", i + 1);
+    putchar('\t');
+
+    for (size_t j = 0; j < exams; j++) {
+      printf("%d\t", studentGradesArray[i][j]);
+    }
+
+    putchar('\n');
+  }
+
+  putchar('\n');
+}
